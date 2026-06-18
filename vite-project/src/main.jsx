4e6f1ebbb253cd5661fabaab7f0211773cbf0ec7
sarans25csc-rgb <sarans25csc@vetias.ac.in>
2026-06-18 10:19:49 +0530

    added main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import react from "react";
+import reactDOM from "react-dom/client";
+import app from "./app";
+reactDOM.createRoot(
+    document.getElementById("root")
+).render(
+    <react.strictmode>
+        <app />
+</react.strictmode>
+);
